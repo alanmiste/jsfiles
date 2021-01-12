6704e4573fe2501ddd405335ff2ae833570e3380
alanmiste <baranalaan@gmail.com>
2021-01-12 14:08:04 +0100

    provide JSON and end point server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const http = require('http'); // we donot need to install HTTP library because it buildin.
 
 const requestListener = function (req, res) { // server callback function - request/response paramiters
-  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all clients to access 
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all clients to access by the *
   res.writeHead(200); //status code for OK
   console.log(Date.now()) // loging in the server not to the browser
   //res.end('Hello, World!');
-  console.log(req.url)
-  res.end(JSON.stringify({todo1:"hello"}))
+  console.log(req.url) // access the request value
+  res.end(JSON.stringify({todo1:"hello"})) // respund with json
 }
 
 const server = http.createServer(requestListener);
